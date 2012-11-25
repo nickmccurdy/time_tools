c504ea11f2e7c62cf752d00c164c1ba0a41275b8
Nicolas McCurdy <thenickperson@gmail.com>
2012-11-24 21:52:09 -0500

    Define elapsedTime() as a class method of Timer

--- scripts.js
+++ scripts.js
@@ -1,31 +1,3 @@
-function elapsedTime(start_time, end_time) {
-	// Calculate the elapsed time and break it down into different units
-	var complete = start_time > end_time;
-	var milliseconds = complete ? start_time-end_time : end_time-start_time;
-	var seconds, minutes, hours, days;
-	seconds = minutes = hours = days = 0;
-	if(Math.abs(milliseconds) >= 1000) {
-		seconds = Math.floor(milliseconds / 1000);
-		milliseconds = milliseconds % 1000;
-		if(Math.abs(seconds) >= 60) {
-			minutes = Math.floor(seconds / 60);
-			seconds = seconds % 60;
-			if(Math.abs(minutes) >= 60) {
-				hours = Math.floor(minutes / 60);
-				minutes = minutes % 60;
-				if(Math.abs(hours) >= 24) {
-					days = Math.floor(hours / 24);
-					hours = hours % 24;
-				}
-			}
-		}
-	}
-	return {
-		'str': days+':'+hours+':'+minutes+':'+seconds+':'+milliseconds,
-		'complete': complete
-	};
-}
-
 Timer = Backbone.Model.extend({
 
 	defaults: {
@@ -43,13 +15,41 @@ Timer = Backbone.Model.extend({
 			start_time = this.get('time');
 			end_time = Date.now();
 		}
-		var elapsed = elapsedTime(Date.now(), this.get('time'));
+		var elapsed = Timer.elapsedTime(Date.now(), this.get('time'));
 		if(this.get('type') === 'countdown' && elapsed.complete) {
 			elapsed.str = '<span class="text-error">'+elapsed.str+' late</span>';
 		}
 		return elapsed.str;
 	}
 
+},{
+	elapsedTime: function(start_time, end_time) {
+		// Calculate the elapsed time and break it down into different units
+		var complete = start_time > end_time;
+		var milliseconds = complete ? start_time-end_time : end_time-start_time;
+		var seconds, minutes, hours, days;
+		seconds = minutes = hours = days = 0;
+		if(Math.abs(milliseconds) >= 1000) {
+			seconds = Math.floor(milliseconds / 1000);
+			milliseconds = milliseconds % 1000;
+			if(Math.abs(seconds) >= 60) {
+				minutes = Math.floor(seconds / 60);
+				seconds = seconds % 60;
+				if(Math.abs(minutes) >= 60) {
+					hours = Math.floor(minutes / 60);
+					minutes = minutes % 60;
+					if(Math.abs(hours) >= 24) {
+						days = Math.floor(hours / 24);
+						hours = hours % 24;
+					}
+				}
+			}
+		}
+		return {
+			'str': days+':'+hours+':'+minutes+':'+seconds+':'+milliseconds,
+			'complete': complete
+		};
+	}
 });
 
 TimerCollection = Backbone.Collection.extend({
