c605fda8dd6dd453ee10d1a972c7423ce4998d21
Nicolas McCurdy <thenickperson@gmail.com>
2014-03-05 02:24:45 -0500

    Use moment for the elapsedTime function

--- model.js
+++ model.js
@@ -16,39 +16,24 @@ Timer = Backbone.Model.extend({
       end_time = Date.now();
     }
     var elapsed = Timer.elapsedTime(Date.now(), this.get('time'));
-    if(this.get('type') === 'countdown' && elapsed.complete) {
-      elapsed.str = '<span class="text-danger">'+elapsed.str+' late</span>';
+    if(this.get('type') === 'countdown' && Timer.isCompleted(start_time, end_time)) {
+      elapsed = '<span class="text-danger">'+elapsed+' late</span>';
     }
-    return elapsed.str;
+    return elapsed;
   }
 
 }, {
+  isCompleted: function(start_time, end_time) {
+    return moment(start_time).isAfter(end_time);
+  },
+
   elapsedTime: function(start_time, end_time) {
-    // Calculate the elapsed time and break it down into different units
-    var complete = start_time > end_time;
-    var milliseconds = complete ? start_time-end_time : end_time-start_time;
-    var seconds, minutes, hours, days;
-    seconds = minutes = hours = days = 0;
-    if(Math.abs(milliseconds) >= 1000) {
-      seconds = Math.floor(milliseconds / 1000);
-      milliseconds = milliseconds % 1000;
-      if(Math.abs(seconds) >= 60) {
-        minutes = Math.floor(seconds / 60);
-        seconds = seconds % 60;
-        if(Math.abs(minutes) >= 60) {
-          hours = Math.floor(minutes / 60);
-          minutes = minutes % 60;
-          if(Math.abs(hours) >= 24) {
-            days = Math.floor(hours / 24);
-            hours = hours % 24;
-          }
-        }
-      }
-    }
-    return {
-      'str': days+':'+hours+':'+minutes+':'+seconds+':'+milliseconds,
-      'complete': complete
-    };
+    var startMoment = moment(start_time);
+    var hours = Math.abs(startMoment.diff(end_time, 'hours'));
+    var minutes = Math.abs(startMoment.diff(end_time, 'minutes'));
+    var seconds = Math.abs(startMoment.diff(end_time, 'seconds', true));
+
+    return hours + ':' + minutes + ':' + seconds;
   }
 });
 
